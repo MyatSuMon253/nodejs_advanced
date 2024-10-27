b0eca5c87922ebe423d2d8523e30064e681fce8b
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-10-27 21:38:28 +0630

    feat: fork children

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+// is the file being executed in master mode
+if (cluster.isMaster) {
+  // cause index.js to be executed again but in child mode
+  cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server and do nothing else
+  const crypto = require("crypto");
+  const express = require("express");
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hello World!");
+  });
+
+  app.listen(3030);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hello World!");
-});
-
-app.listen(3030);
